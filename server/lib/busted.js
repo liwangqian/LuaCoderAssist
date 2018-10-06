fcc56704972b50c7ac9b7a8c68d3b56bcad55e78
liwangqian <wqgg123@163.com>
2018-10-06 22:54:27 +0800

    优化静态检查逻辑，防止多个检查进程同时检查同一个文件的不同版本，确保最新修改被检查到

--- server/lib/busted.js
+++ server/lib/busted.js
@@ -4,11 +4,11 @@ const engine = require('./engine');
 
 const BUSTED_PRELOAD = 'stdlibs/busted.json';
 
-let _std_assert = undefined;
-let _busted_mode = false;
+let _stdAssert = undefined;
+let _bustedMode = false;
 
 function enterBustedMode(extensionPath) {
-    _std_assert = engine._G.get('assert');
+    _stdAssert = engine._G.get('assert');
     engine._G.set('assert', undefined, true);
     const busted = engine.LoadedPackages['busted'];
     if (busted) {
@@ -17,19 +17,19 @@ function enterBustedMode(extensionPath) {
     } else {
         engine.loadExtentLib(extensionPath + BUSTED_PRELOAD, "busted");
     }
-    _busted_mode = true;
+    _bustedMode = true;
 }
 
 exports.enterBustedMode = enterBustedMode;
 
 function exitBustedMode() {
-    if (!_busted_mode) {
+    if (!_bustedMode) {
         return;
     }
     const busted = engine.LoadedPackages['busted'];
     busted.state.valid = false;
     clearInvalidSymbols();
-    engine._G.set('assert', _std_assert, true);
+    engine._G.set('assert', _stdAssert, true);
 }
 
 function clearInvalidSymbols() {
--- server/providers/diagnostic-provider.js
+++ server/providers/diagnostic-provider.js
@@ -8,23 +8,29 @@ class DiagnosticProvider {
     constructor(coder) {
         this.coder = coder;
         this.linters = [];
-        this._lintQueue = {};
+        this.lastTasks = {};
 
         this._initLinters();
     }
 
     provideDiagnostics(uri) {
-        if (this._lintQueue[uri]) {
-            // has pending linter task, so just return.
-            return;
-        }
+        this._ensureTask(uri);
+        this._updateTask(uri, this._newTimeoutTask(uri));
+    }
 
-        // fire after a while wait for more input, performance consideration
-        // this._lintTask(uri);
-        this._lintQueue[uri] = setTimeout((uri) => {
-            this._lintQueue[uri] = undefined;
+    _updateTask(uri, newTimer) {
+        let lastTimer = this.lastTasks[uri].timer;
+        if (lastTimer) {
+            /*定时器还没超时，则取消，重新起定时器*/
+            clearTimeout(lastTimer);
+        }
+        this.lastTasks[uri].timer = newTimer;
+    }
+
+    _newTimeoutTask(uri) {
+        return setTimeout((uri) => {
             this._lintTask(uri);
-        }, 100, uri);
+        }, 200, uri);
     }
 
     _initLinters() {
@@ -33,17 +39,24 @@ class DiagnosticProvider {
 
     _lintTask(uri) {
         return awaiter.await(this, void 0, void 0, function* () {
+            const lastTask = this.lastTasks[uri];
+            lastTask.timer = undefined;
+
             const document = yield this.coder.document(uri);
+            const version = document.version;
+            if (version === lastTask.version) {
+                /*没有新的修改*/
+                return;
+            }
+            lastTask.version = version;
             const text = document.getText();
-            this.linters.forEach(linter => {
-                // linters pre-condition check
+            this.linters.forEach((linter, idx) => {
                 if (linter.precondiction && !linter.precondiction(document)) {
                     return;
                 }
 
                 const command = linter.command(document);
-
-                this._lint(command, text).then(ok => { }, nok => {
+                this._run(uri, idx, command, text).then(() => { }, nok => {
                     const diagnostics = linter.parseDiagnostics(nok);
                     this.coder.sendDiagnostics(uri, diagnostics);
                 });
@@ -51,8 +64,23 @@ class DiagnosticProvider {
         });
     }
 
-    _lint(linter, input) {
+    _ensureTask(uri) {
+        if (!this.lastTasks[uri]) {
+            this.lastTasks[uri] = {
+                version: 0,
+                timer: undefined,
+                procs: []
+            };
+        }
+    }
+
+    _run(uri, idx, linter, input) {
         return new Promise((resolve, reject) => {
+            /*如果上一次检查的进程未结束，则先杀掉老进程*/
+            let procs = this.lastTasks[uri].procs;
+            procs[idx] && procs[idx].kill();
+
+            /*重新创建一个检查进程*/
             let proc = execFile(linter.cmd, linter.args, { cwd: linter.cwd }, (error, stdout, stderr) => {
                 if (error != null) {
                     reject({ error: error, stdout: stdout, stderr: stderr });
@@ -61,6 +89,7 @@ class DiagnosticProvider {
                 }
             });
 
+            procs[idx] = proc;
             proc.stdin.end(input);
         });
     }
